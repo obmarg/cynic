559166afaeafded09de6492e2260b5fc42bc88ab
Graeme Coupar <grambo@grambo.me.uk>
2020-07-06 23:29:47 +0100

    Fix formatting

--- cynic-querygen/tests/starwars-querygen.rs
+++ cynic-querygen/tests/starwars-querygen.rs
@@ -9,13 +9,18 @@ macro_rules! test_query_file {
             let schema = include_str!("../../examples/examples/starwars.schema.graphql");
             let query = include_str!($filename);
 
-            assert_snapshot!(
-                document_to_fragment_structs(query, schema, &QueryGenOptions::default())
-                    .expect("QueryGen Failed")
+            assert_snapshot!(document_to_fragment_structs(
+                query,
+                schema,
+                &QueryGenOptions::default()
             )
+            .expect("QueryGen Failed"))
         }
-    }
+    };
 }
 
-test_query_file!(sanity_test_starwars_query, "queries/starwars-sanity.graphql");
+test_query_file!(
+    sanity_test_starwars_query,
+    "queries/starwars-sanity.graphql"
+);
 test_query_file!(test_nested_arguments, "queries/nested-arguments.graphql");
