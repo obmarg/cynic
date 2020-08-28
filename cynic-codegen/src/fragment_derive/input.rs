c9b79a4acea37fc5b3087d83798ec0ee0052fde9
Graeme Coupar <grambo@grambo.me.uk>
2020-08-28 20:06:55 +0100

    Removed optimised query modules (#100)
    #### Why are we making this change?
    
    At one point the query_module attribute called into the other derive attrs
    itself so as to avoid having to parse the GQL schema several times. This didn't
    seem to have much performance impact, but did make error reporting 
    significantly worse - all the errors in a module pointed to the attr, rather
    than at least the struct they came from. So I put this impl behind a feature
    flag.
    
    Later on I was making some changes that added a new path to the optimised query
    module feature in addition to an impl outwith it.  I'm not convinced the
    feature is that useful, so I removed it rather than adding this new path to it.
    
    #### What effects does this change have?
    
    Strips out the code that powered the feature.  The original feature flag was
    removed in #95.
    
    Fixes #98

--- cynic-codegen/src/fragment_derive/input.rs
+++ cynic-codegen/src/fragment_derive/input.rs
@@ -24,39 +24,3 @@ pub struct FragmentDeriveField {
     #[darling(default)]
     pub(super) flatten: bool,
 }
-
-/// An alternative FragmentDeriveInput struct that doesn't require as many fields.
-///
-/// This is used by the query_module generation, which provides some of the parameters
-/// without the users input.
-#[derive(darling::FromDeriveInput)]
-#[darling(attributes(cynic), supports(struct_named))]
-pub(crate) struct QueryModuleFragmentDeriveInput {
-    ident: proc_macro2::Ident,
-    data: darling::ast::Data<(), crate::fragment_derive::FragmentDeriveField>,
-
-    #[darling(default)]
-    pub schema_path: Option<SpannedValue<String>>,
-    #[darling(default)]
-    pub query_module: Option<SpannedValue<String>>,
-    pub graphql_type: SpannedValue<String>,
-    #[darling(default)]
-    pub argument_struct: Option<syn::Ident>,
-}
-
-impl QueryModuleFragmentDeriveInput {
-    pub fn to_fragment_derive_input(
-        self,
-        schema_path: &SpannedValue<String>,
-        query_module: &SpannedValue<String>,
-    ) -> FragmentDeriveInput {
-        FragmentDeriveInput {
-            ident: self.ident,
-            data: self.data,
-            schema_path: self.schema_path.unwrap_or_else(|| schema_path.clone()),
-            query_module: self.query_module.unwrap_or_else(|| query_module.clone()),
-            graphql_type: self.graphql_type,
-            argument_struct: self.argument_struct,
-        }
-    }
-}
