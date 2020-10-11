6830dab8de11f4fef8ab9871c1654b33b70ff3e6
Anthony Dodd <Dodd.AnthonyJosiah@gmail.com>
2020-10-11 08:42:16 -0500

    Finish building out the GraphQLError type. (#129)
    #### Why are we making this change?
    
    These changes are being made to finish up the remaining work on the
    GraphQLError type. It is now complete, and allows for full transmission of
    error data from the server side to the client side.
    
    #### What effects does this change have?
    
    This type is now complete according to the GraphQL spec, and includes proper
    data models for locations, path & the extensions field. All of these fields are
    optional, however their shape is well defined when included. The extensions
    field is defined as being an arbitrarily shaped JSON object, and as such is
    modeled as an `Option<serde_json::Value>`.
    
    Also added the feature `surf-wasm-client` feature which will enable the
    `surf/wasm-client` passthrough feature so that the surf feature may be used in
    the wasm32 web context.
    
    This change is fully backwards compatible for all GraphQL spec compliant
    servers. All of the added fields are optional, as defined in the spec. Overall,
    this change will allow for the full use of GraphQL's error handling mechanisms,
    which my production app, and many others, rely upon heavily.
    
    A sample of what this error response object looks like in action (this is an
    error response from a Juniper GraphQL API too):
    
    ```
    GraphQLResponse {
        data: None,
        errors: Some([GraphQLError {
            message: "Invalid verification code provided.",
            locations: Some([GraphQLErrorLocation { line: 2, column: 3 }]),
            path: Some([Field("federateLogin")]),
            extensions: Some(Object({"status": String("400")}))
        }])
    }
    ```
    
    Closes #127

--- cynic/src/result.rs
+++ cynic/src/result.rs
@@ -28,13 +28,57 @@ impl<T> GraphQLResponse<T> {
     }
 }*/
 
-#[derive(Debug, serde::Deserialize, thiserror::Error)]
+/// A model describing an error which has taken place during execution.
+#[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize, thiserror::Error)]
 #[error("{message}")]
 pub struct GraphQLError {
-    message: String,
+    /// A description of the error which has taken place.
+    pub message: String,
+    /// Optional description of the locations where the errors have taken place.
+    pub locations: Option<Vec<GraphQLErrorLocation>>,
+    /// Optional path to the response field which experienced the associated error.
+    pub path: Option<Vec<GraphQLErrorPathSegment>>,
+    /// Optional arbitrary JSON data describing the error in more detail.
+    pub extensions: Option<serde_json::Value>,
 }
 
-/// The data returned by a GraphQL query when the query had errors.   
+impl GraphQLError {
+    /// Construct a new instance.
+    pub fn new(
+        message: String,
+        locations: Option<Vec<GraphQLErrorLocation>>,
+        path: Option<Vec<GraphQLErrorPathSegment>>,
+        extensions: Option<serde_json::Value>,
+    ) -> Self {
+        GraphQLError {
+            message,
+            locations,
+            path,
+            extensions,
+        }
+    }
+}
+
+/// A line and column offset describing the location of an error within a GraphQL document.
+#[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize)]
+pub struct GraphQLErrorLocation {
+    /// The line at which the associated error begins.
+    pub line: i32,
+    /// The column of the line at which the associated error begins.
+    pub column: i32,
+}
+
+/// A segment of a GraphQL error path.
+#[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize)]
+#[serde(untagged)]
+pub enum GraphQLErrorPathSegment {
+    /// A path segment representing a field by name.
+    Field(String),
+    /// A path segment representing an index offset, zero-based.
+    Index(i32),
+}
+
+/// The data returned by a GraphQL query when the query had errors.
 /// GraphQL allows servers to return partial data in this case, but if there's
 /// missing fields that aren't represented by an Option we won't have been
 /// able to decode that data.
