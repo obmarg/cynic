2f5a2aaf716700622ac1a091bd3d76803d04824e
Graeme Coupar <grambo@grambo.me.uk>
2020-12-30 22:45:20 +0000

    Some more Operation docs

--- cynic/src/operation.rs
+++ cynic/src/operation.rs
@@ -6,6 +6,11 @@ use crate::{
     Argument, GraphQLResponse, MutationRoot, QueryRoot, SelectionSet,
 };
 
+/// An Operation that can be sent to a remote GraphQL server.
+///
+/// This contains a GraphQL query string and variable HashMap.  It can be
+/// serialized into JSON with `serde::Serialize` and sent to a remote server,
+/// and has a `decode_response` function that knows how to decode a response.
 #[derive(serde::Serialize)]
 pub struct Operation<'a, ResponseData> {
     pub query: String,
@@ -15,6 +20,7 @@ pub struct Operation<'a, ResponseData> {
 }
 
 impl<'a, ResponseData: 'a> Operation<'a, ResponseData> {
+    /// Constructs a new Operation from a query `SelectionSet`
     pub fn query<Root: QueryRoot>(selection_set: SelectionSet<'a, ResponseData, Root>) -> Self {
         let (query, arguments, decoder) = query_root(selection_set).query_arguments_and_decoder();
 
@@ -31,6 +37,7 @@ impl<'a, ResponseData: 'a> Operation<'a, ResponseData> {
         }
     }
 
+    /// Constructs a new Operation from a mutation `SelectionSet`
     pub fn mutation<Root: MutationRoot>(
         selection_set: SelectionSet<'a, ResponseData, Root>,
     ) -> Self {
@@ -50,6 +57,8 @@ impl<'a, ResponseData: 'a> Operation<'a, ResponseData> {
         }
     }
 
+    /// Decodes a response.  Note that you need to decode a GraphQLResponse
+    /// from JSON before passing to this function
     pub fn decode_response(
         &self,
         response: GraphQLResponse<serde_json::Value>,
