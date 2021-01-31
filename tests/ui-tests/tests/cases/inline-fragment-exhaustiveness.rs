bacfebfb66b877b68577bb0e6038f843cba5fa5a
Graeme Coupar <grambo@grambo.me.uk>
2021-01-31 17:35:14 +0000

    Commit ui tests

---
+++ tests/ui-tests/tests/cases/inline-fragment-exhaustiveness.rs
@@ -0,0 +1,39 @@
+fn main() {}
+
+mod query_dsl {
+    type Json = serde_json::Value;
+
+    cynic::query_dsl!("../../../cynic/src/bin/simple.graphql");
+}
+
+#[derive(cynic::InlineFragments)]
+#[cynic(
+    schema_path = "../../../cynic/src/bin/simple.graphql",
+    query_module = "query_dsl",
+    graphql_type = "MyUnionType"
+)]
+enum MyUnionType {
+    Test(Test),
+    Nested(Nested),
+}
+
+#[derive(cynic::QueryFragment)]
+#[cynic(
+    schema_path = "../../../cynic/src/bin/simple.graphql",
+    query_module = "query_dsl",
+    graphql_type = "Nested"
+)]
+struct Nested {
+    pub a_string: String,
+    pub opt_string: Option<String>,
+}
+
+#[derive(cynic::QueryFragment)]
+#[cynic(
+    schema_path = "../../../cynic/src/bin/simple.graphql",
+    query_module = "query_dsl",
+    graphql_type = "TestStruct"
+)]
+struct Test {
+    pub field_one: String,
+}
---
+++ tests/ui-tests/tests/ui-tests.rs
@@ -0,0 +1,6 @@
+#[test]
+fn ui_test_inlinefragments() {
+    let t = trybuild::TestCases::new();
+    t.compile_fail("tests/cases/inline-fragment-exhaustiveness.rs");
+    t.compile_fail("tests/cases/inline-fragment-fallback-validation.rs");
+}
