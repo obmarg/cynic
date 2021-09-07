a15fe60a7b243308660c1820cc3ed7c325374426
Graeme Coupar <grambo@grambo.me.uk>
2021-09-07 22:53:04 +0000

    Attempt to add CI caching via rust-cache (#265)
    #### Why are we making this change?
    
    I read [this post](https://matklad.github.io/2021/09/04/fast-rust-builds.html) today and wanted to implement some of it's suggestions.
    
    #### What effects does this change have?
    
    - Speeds up CI
    - Introduces caching in CI
    - Introduces bors

--- tests/querygen-compile-run/src/lib.rs
+++ tests/querygen-compile-run/src/lib.rs
@@ -1,15 +1,15 @@
+use serde_json::json;
+
 pub fn send<'a, ResponseData: 'a>(
     url: &str,
     operation: cynic::Operation<'a, ResponseData>,
 ) -> Result<ResponseData, Box<dyn std::error::Error>> {
-    let response = reqwest::blocking::Client::new()
-        .post(url)
-        .header("User-Agent", "obmarg/cynic")
-        .json(&operation)
-        .send()
+    let response = ureq::post(url)
+        .set("User-Agent", "obmarg/cynic")
+        .send_json(json!(operation))
         .unwrap();
 
-    let response_data = operation.decode_response(response.json()?)?;
+    let response_data = operation.decode_response(response.into_json()?)?;
     if let Some(_errors) = response_data.errors {
         println!("{:?}", _errors);
         return Err("GraphQL server returned errors".into());
