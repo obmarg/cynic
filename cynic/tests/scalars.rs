20d0cce3e2cb695e3a272aa6c31d8163d431f5b0
Graeme Coupar <grambo@grambo.me.uk>
2022-02-20 19:58:30 +0000

    Fix the scalar derive. (#401)
    #### Why are we making this change?
    
    #391 changed the way scalar derives worked, but it didn't handle generic types inside a scalar derive.
    
    #### What effects does this change have?
    
    Fixes generic types in a scalar derive, adds a test.

---
+++ cynic/tests/scalars.rs
@@ -0,0 +1,10 @@
+mod schema {
+    cynic::use_schema!("tests/test-schema.graphql");
+}
+
+// Make sure we can derive DateTime
+#[derive(cynic::Scalar)]
+pub struct DateTime(pub chrono::DateTime<chrono::Utc>);
+
+// Make sure we can impl_scalar for third party types.
+cynic::impl_scalar!(chrono::DateTime<chrono::Utc>, schema::DateTime);
