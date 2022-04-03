a39512ae46231cc4934f46ef8bda8a7a05a61577
Graeme Coupar <grambo@grambo.me.uk>
2022-04-03 19:10:07 +0100

    cargo install linkcheck

--- cynic-book/build.sh
+++ cynic-book/build.sh
@@ -7,12 +7,14 @@ cd cynic-book
 wget https://github.com/rust-lang/mdBook/releases/download/v0.4.15/mdbook-v0.4.15-x86_64-unknown-linux-gnu.tar.gz
 tar xvzf mdbook-v0.4.15-x86_64-unknown-linux-gnu.tar.gz
 
-mkdir -p mdbook-linkcheck && cd "$_" && \
-  curl -L https://github.com/Michael-F-Bryan/mdbook-linkcheck/releases/latest/download/mdbook-linkcheck.x86_64-unknown-linux-gnu.zip -o mdbook-linkcheck.zip && \
-  unzip "$_" && \
-  chmod +x mdbook-linkcheck && \
-  export PATH=$PWD:$PATH && \
-  cd ..
+# mkdir -p mdbook-linkcheck && cd "$_" && \
+  # curl -L https://github.com/Michael-F-Bryan/mdbook-linkcheck/releases/latest/download/mdbook-linkcheck.x86_64-unknown-linux-gnu.zip -o mdbook-linkcheck.zip && \
+  # unzip "$_" && \
+  # chmod +x mdbook-linkcheck && \
+  # export PATH=$PWD:$PATH && \
+  # cd ..
+
+cargo install mdbook-linkcheck
 
 ./mdbook build
 
