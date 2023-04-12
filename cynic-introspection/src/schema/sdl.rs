e50e661012f8e9e0486f9b1fc133fe3d156e52ca
Graeme Coupar <grambo@grambo.me.uk>
2023-04-12 21:45:54 +0100

    Even more schema output

--- cynic-introspection/src/schema/sdl.rs
+++ cynic-introspection/src/schema/sdl.rs
@@ -1,22 +1,22 @@
 use std::fmt::{self, Display, Write};
 
-use cynic::InputObject;
-
 use crate::{
-    EnumType, Field, FieldType, InputObjectType, InterfaceType, ObjectType, ScalarType, Type,
-    UnionType, WrappingType,
+    Deprecated, EnumType, EnumValue, Field, FieldType, FieldWrapping, InputObjectType, InputValue,
+    InterfaceType, ObjectType, ScalarType, Type, UnionType, WrappingType,
 };
 
 use super::Schema;
 
 impl Schema {
-    pub fn write_sdl(&self, f: &dyn Write) -> std::io::Result<()> {
-        todo!()
+    /// Writes the SDL for this schema into `f`
+    pub fn write_sdl(&self, f: &mut dyn Write) -> std::fmt::Result {
+        write!(f, "{}", SchemaDisplay(self))
     }
 
+    /// Returns the SDL for this schema as a string
     pub fn to_sdl(&self) -> String {
         let mut output = String::new();
-        self.write_sdl(&output)
+        self.write_sdl(&mut output)
             .expect("Writing to a string shouldn't fail");
         output
     }
@@ -56,10 +56,24 @@ impl std::fmt::Display for TypeDisplay<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let ty = self.0;
 
+        if ty.name().starts_with("__") {
+            // Don't render introspection types.
+            return Ok(());
+        }
+
         match ty {
-            Type::Scalar(ScalarType { name, description }) => {
-                writeln!(f, "{}", DescriptionDisplay(description.as_deref()))?;
-                writeln!(f, "scalar {name}")
+            Type::Scalar(ScalarType {
+                name,
+                description,
+                specified_by_url,
+            }) => {
+                // TODO: probably skip builtins here...
+                write!(f, "{}", DescriptionDisplay(description.as_deref()))?;
+                writeln!(
+                    f,
+                    "scalar {name}{}",
+                    SpecifiedByDisplay(specified_by_url.as_deref())
+                )
             }
             Type::Object(ObjectType {
                 name,
@@ -67,10 +81,10 @@ impl std::fmt::Display for TypeDisplay<'_> {
                 fields,
                 interfaces,
             }) => {
-                writeln!(f, "{}", DescriptionDisplay(description.as_deref()))?;
-                writeln!(f, "type {name} {{")?;
+                write!(f, "{}", DescriptionDisplay(description.as_deref()))?;
+                writeln!(f, "type {name}{} {{", ImplementsDisplay(interfaces))?;
                 for field in fields {
-                    writeln!(f, "{}", FieldDisplay(field))?;
+                    writeln!(indented(f), "{}", FieldDisplay(field))?;
                 }
                 writeln!(f, "}}")
             }
@@ -78,24 +92,55 @@ impl std::fmt::Display for TypeDisplay<'_> {
                 name,
                 description,
                 fields,
-            }) => todo!(),
+            }) => {
+                write!(f, "{}", DescriptionDisplay(description.as_deref()))?;
+                writeln!(f, "input {name} {{")?;
+                for field in fields {
+                    writeln!(indented(f), "{}", InputValueDisplay(field))?;
+                }
+                writeln!(f, "}}")
+            }
             Type::Enum(EnumType {
                 name,
                 description,
                 values,
-            }) => todo!(),
+            }) => {
+                write!(f, "{}", DescriptionDisplay(description.as_deref()))?;
+                writeln!(f, "enum {name} {{")?;
+                for value in values {
+                    writeln!(indented(f), "{}", EnumValueDisplay(value))?;
+                }
+                writeln!(f, "}}")
+            }
             Type::Interface(InterfaceType {
                 name,
                 description,
                 fields,
                 interfaces,
-                possible_types,
-            }) => todo!(),
+                possible_types: _,
+            }) => {
+                write!(f, "{}", DescriptionDisplay(description.as_deref()))?;
+                writeln!(f, "interface {name}{} {{", ImplementsDisplay(interfaces))?;
+                for field in fields {
+                    writeln!(indented(f), "{}", FieldDisplay(field))?;
+                }
+                writeln!(f, "}}")
+            }
             Type::Union(UnionType {
                 name,
                 description,
                 possible_types,
-            }) => todo!(),
+            }) => {
+                write!(f, "{}", DescriptionDisplay(description.as_deref()))?;
+                write!(f, "union {name} = ")?;
+                for (i, ty) in possible_types.iter().enumerate() {
+                    if i != 0 {
+                        write!(f, " | ")?;
+                    }
+                    write!(f, "{ty}")?;
+                }
+                Ok(())
+            }
         }
     }
 }
@@ -125,7 +170,42 @@ impl Display for FieldDisplay<'_> {
             ty,
             deprecated,
         } = self.0;
-        writeln!(f, "{name}: {}", FieldTypeDisplay(ty))
+        write!(f, "{}", DescriptionDisplay(description.as_deref()))?;
+        write!(f, "{name}")?;
+        if !args.is_empty() {
+            writeln!(f, "(")?;
+            for arg in args {
+                writeln!(indented(f), "{}", InputValueDisplay(arg))?;
+            }
+            write!(f, ")")?;
+        }
+        write!(
+            f,
+            ": {} {}",
+            FieldTypeDisplay(ty),
+            DeprecatedDisplay(deprecated)
+        )?;
+
+        Ok(())
+    }
+}
+
+struct ImplementsDisplay<'a>(&'a [String]);
+
+impl Display for ImplementsDisplay<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let interfaces = &self.0;
+        if interfaces.is_empty() {
+            return Ok(());
+        }
+        write!(f, " implements ")?;
+        for (i, interface) in interfaces.iter().enumerate() {
+            if i != 0 {
+                write!(f, " & ")?;
+            }
+            write!(f, "{interface}")?;
+        }
+        Ok(())
     }
 }
 
@@ -134,6 +214,74 @@ struct FieldTypeDisplay<'a>(&'a FieldType);
 impl Display for FieldTypeDisplay<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let FieldType { wrapping, name } = self.0;
+        write!(f, "{}", WrappingDisplay(wrapping, name))
+    }
+}
+
+struct InputValueDisplay<'a>(&'a InputValue);
+
+impl Display for InputValueDisplay<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let InputValue {
+            name,
+            description,
+            ty,
+            default_value,
+        } = self.0;
+        write!(f, "{}", DescriptionDisplay(description.as_deref()))?;
+        write!(f, "{name}: {}", FieldTypeDisplay(ty))?;
+        if let Some(default_value) = default_value {
+            write!(f, " = {}", default_value)?;
+        }
+        writeln!(f)
+    }
+}
+
+struct EnumValueDisplay<'a>(&'a EnumValue);
+
+impl Display for EnumValueDisplay<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let EnumValue {
+            name,
+            description,
+            deprecated,
+        } = self.0;
+        write!(f, "{}", DescriptionDisplay(description.as_deref()))?;
+        write!(f, "{name}")?;
+        writeln!(f, "{}", DeprecatedDisplay(deprecated))
+    }
+}
+
+struct DeprecatedDisplay<'a>(&'a Deprecated);
+
+impl Display for DeprecatedDisplay<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.0 {
+            Deprecated::No => {}
+            Deprecated::Yes(None) => write!(f, "@deprecated")?,
+            Deprecated::Yes(Some(reason)) => write!(f, "@deprecated(reason: {reason})")?,
+        }
+        Ok(())
+    }
+}
+
+struct SpecifiedByDisplay<'a>(Option<&'a str>);
+
+impl Display for SpecifiedByDisplay<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.0 {
+            None => {}
+            Some(url) => write!(f, " @specifiedBy(url: {url})")?,
+        }
+        Ok(())
+    }
+}
+
+struct WrappingDisplay<'a>(&'a FieldWrapping, &'a str);
+
+impl Display for WrappingDisplay<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let WrappingDisplay(wrapping, inner_text) = self;
         let wrapping_types = wrapping.into_iter().collect::<Vec<_>>();
         for wrapping_type in &wrapping_types {
             match wrapping_type {
@@ -141,14 +289,13 @@ impl Display for FieldTypeDisplay<'_> {
                 WrappingType::NonNull => {}
             }
         }
-        writeln!(f, "{name}")?;
+        write!(f, "{inner_text}")?;
         for wrapping_type in wrapping_types.iter().rev() {
             match wrapping_type {
                 WrappingType::List => write!(f, "]")?,
                 WrappingType::NonNull => write!(f, "?")?,
             }
         }
-
         Ok(())
     }
 }
