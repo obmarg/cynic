ced463296c77528584cca0b979b2e328e4b6df3d
Graeme Coupar <grambo@grambo.me.uk>
2023-05-07 14:35:50 +0100

    Types -> OptimisedTypes

--- cynic-codegen/src/schema/type_index/optimised.rs
+++ cynic-codegen/src/schema/type_index/optimised.rs
@@ -1,18 +1,16 @@
+//! TODO: Docstring
 use std::collections::HashMap;
 
 use crate::schema::{self, types::Type, Schema};
 
 #[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, PartialEq, Eq, Debug)]
 #[archive(check_bytes)]
-// TODO: Convert into this somehow,
-// then serialize
-// then write code to load an ArchivedTypes and deserialize
-// on appropriate calls...
-struct Types<'a>(HashMap<String, Type<'a>>);
+/// TODO: Doc string
+pub struct OptimisedTypes<'a>(HashMap<String, Type<'a>>);
 
 impl Schema<'_, schema::Validated> {
-    fn optimise<'a>(&'a self) -> Types<'a> {
-        Types(
+    fn optimise<'a>(&'a self) -> OptimisedTypes<'a> {
+        OptimisedTypes(
             self.type_index
                 .unsafe_iter()
                 .map(|ty| (ty.name().to_string(), ty))
@@ -41,12 +39,12 @@ mod tests {
         optimised
             .0
             .iter()
-            .for_each(|(name, ty)| assert_eq!(schema.lookup::<Type<'_>>(&name).unwrap(), *ty));
+            .for_each(|(name, ty)| assert_eq!(schema.lookup::<Type<'_>>(name).unwrap(), *ty));
 
         let bytes = rkyv::to_bytes::<_, 1024>(&optimised).unwrap();
-        let archived = rkyv::check_archived_root::<Types<'_>>(&bytes[..]).unwrap();
+        let archived = rkyv::check_archived_root::<OptimisedTypes<'_>>(&bytes[..]).unwrap();
 
-        let deserialized: Types<'_> = archived.deserialize(&mut rkyv::Infallible).unwrap();
+        let deserialized: OptimisedTypes<'_> = archived.deserialize(&mut rkyv::Infallible).unwrap();
 
         assert_eq!(deserialized, optimised);
     }
