6d214140952b5f650c3ceb4853fed7e72eabfd79
Graeme Coupar <graeme@turbofish.tech>
2023-06-09 12:27:19 +0100

    Add a test of generics in response types

--- cynic/tests/generics_simple.rs
+++ cynic/tests/generics_simple.rs
@@ -32,3 +32,33 @@ fn test_query_building() {
 
     insta::assert_snapshot!(operation.query);
 }
+
+#[derive(cynic::QueryFragment, PartialEq, Debug)]
+#[cynic(
+    schema_path = "src/bin/simple.graphql",
+    graphql_type = "Query",
+    variables = "TestArgs"
+)]
+struct GenericInResponse<T>
+where
+    T: cynic::QueryFragment<SchemaType = schema::TestStruct>,
+    TestArgsFields: cynic::queries::VariableMatch<T::VariablesFields>,
+{
+    test_struct: Option<T>,
+}
+
+#[test]
+fn test_generic_in_response() {
+    use cynic::QueryBuilder;
+
+    let operation = GenericInResponse::<TestStruct>::build(TestArgs { a_str: Some("1") });
+
+    insta::assert_snapshot!(operation.query, @r###"
+    query GenericInResponse($aStr: String) {
+      testStruct {
+        fieldOne(x: 1, y: $aStr)
+      }
+    }
+
+    "###);
+}
