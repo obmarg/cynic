7e7b50f7b98b1aa574fe1af1d32eec89651c0491
Graeme Coupar <graeme@turbofish.tech>
2023-09-12 21:46:04 +0100

    run compile benchmark for 2023
    A good improvement, though this is entirely unscientific: I'm running a better
    machine now than I was when I made the last benchmark.

--- tests/compile-benchmarks/bench.sh
+++ tests/compile-benchmarks/bench.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 cd github-query
-cargo build 
+cargo build
 cargo build --release
 
 GIT_SHA=$(git show -s --format=%H)
 RUST_VERSION=$(rustc --version | cut -f2 -d" ")
 DATE=$(date +%F)
 
-hyperfine --export-json "../timings/github-query/$DATE.$GIT_SHA.$RUST_VERSION.debug.json" -p "cargo clean -p queries" "cargo build -p queries" 
-hyperfine --export-json "../timings/github-query/$DATE.$GIT_SHA.$RUST_VERSION.release.json" -p "cargo clean --release -p queries" "cargo build --release -p queries" 
+hyperfine --export-json "../timings/github-query/$DATE.$GIT_SHA.$RUST_VERSION.debug.json" -p "cargo clean -p queries" "cargo build -p queries"
+hyperfine --export-json "../timings/github-query/$DATE.$GIT_SHA.$RUST_VERSION.release.json" -p "cargo clean --release -p queries" "cargo build --release -p queries"
