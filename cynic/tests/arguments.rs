0887c847fae1052f77cfc44c2103473586e08d49
Graeme Coupar <grambo@grambo.me.uk>
2024-01-22 10:19:45 +0000

    Changelog & a test for #835 (#837)

---
+++ cynic/tests/arguments.rs
@@ -0,0 +1,43 @@
+//! Tests of the generated serialization code for InputObjects
+
+use serde::Serialize;
+
+mod schema {
+    cynic::use_schema!("tests/test-schema.graphql");
+}
+
+#[test]
+fn test_literal_object_inside_list() {
+    use cynic::QueryBuilder;
+
+    #[derive(cynic::QueryFragment, Serialize)]
+    #[cynic(graphql_type = "BlogPost", schema_path = "tests/test-schema.graphql")]
+    struct Post {
+        has_metadata: Option<bool>,
+    }
+
+    #[derive(cynic::QueryFragment)]
+    #[cynic(schema_path = "tests/test-schema.graphql")]
+    struct Query {
+        #[allow(dead_code)]
+        #[arguments(filters: { any: [{states: DRAFT}] })]
+        filtered_posts: Vec<BlogPost>,
+    }
+
+    let query = Query::build(());
+
+    insta::assert_display_snapshot!(query.query, @r###"
+    query Query {
+      filteredPosts(filters: {any: [{states: [DRAFT, ], }, ], }) {
+        hasMetadata
+      }
+    }
+
+    "###);
+}
+
+#[derive(cynic::QueryFragment, Serialize)]
+#[cynic(schema_path = "tests/test-schema.graphql")]
+struct BlogPost {
+    has_metadata: Option<bool>,
+}
