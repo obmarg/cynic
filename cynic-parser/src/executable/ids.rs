21282f282570adeecd801228f37bddc03b422c26
Graeme Coupar <graeme@turbofish.tech>
2024-03-24 13:50:54 +0000

    cargo fmt

--- cynic-parser/src/executable/ids.rs
+++ cynic-parser/src/executable/ids.rs
@@ -1,9 +1,6 @@
 use std::num::NonZeroU32;
 
-use super::{
-    storage::*, types::TypeRecord, value::ValueRecord,
-    ExecutableDocument,
-};
+use super::{storage::*, types::TypeRecord, value::ValueRecord, ExecutableDocument};
 use crate::{common::IdRange, AstLookup};
 
 macro_rules! make_id {
