30ea2a81b94a31fc1e29bc903a5f4ba95a299349
Graeme Coupar <grambo@grambo.me.uk>
2024-05-11 17:18:38 +0100

    fix: directive location panic in cynic-parser (#948)

--- cynic-parser/tests/report.rs
+++ cynic-parser/tests/report.rs
@@ -18,3 +18,23 @@ fn test_report() {
     ───╯
     "###);
 }
+
+#[test]
+fn test_invalid_directive_location() {
+    let document = "directive @Blah on BLAH";
+
+    let report = cynic_parser::parse_type_system_document(document)
+        .map(|_| ())
+        .unwrap_err()
+        .to_report(document);
+
+    insta::assert_display_snapshot!(report, @r###"
+    [03] Error: unknown directive location: BLAH. expected one of QUERY, MUTATION, SUBSCRIPTION, FIELD, FRAGMENT_DEFINITION, FRAGMENT_SPREAD, INLINE_FRAGMENT, SCHEMA, SCALAR, OBJECT, FIELD_DEFINITION, ARGUMENT_DEFINITION, INTERFACE, UNION, ENUM, ENUM_VALUE, INPUT_OBJECT, INPUT_FIELD_DEFINITION, VARIABLE_DEFINITION
+       ╭─[<unknown>:1:1]
+       │
+     1 │ directive @Blah on BLAH
+       │                    ──┬─  
+       │                      ╰─── this is not a valid directive location
+    ───╯
+    "###);
+}
