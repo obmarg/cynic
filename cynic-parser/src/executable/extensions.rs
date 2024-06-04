dc1e22f82f51e077d397ccdf695721ce87e84a05
Graeme Coupar <grambo@grambo.me.uk>
2024-06-04 13:50:06 +0100

    fix: missing lifetime on `FragmentSpread::fragment` (#978)

--- cynic-parser/src/executable/extensions.rs
+++ cynic-parser/src/executable/extensions.rs
@@ -1,7 +1,7 @@
 use super::{FragmentDefinition, FragmentSpread};
 
 impl<'a> FragmentSpread<'a> {
-    pub fn fragment(&self) -> Option<FragmentDefinition> {
+    pub fn fragment(&self) -> Option<FragmentDefinition<'a>> {
         let document = self.0.document;
         let needle = self.fragment_name();
 
